10376b37948cfd0fdcc29b8d855c453798ec6b89
Wojciech Knott <wyoyo7@gmail.com>
2020-05-24 15:12:46 +0200

    Add books routes to api

--- server/index.js
+++ server/index.js
@@ -3,10 +3,15 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to MongoDB'))
+
+app.use(express.json())
+
+const booksRouter = require('./routes/books')
+app.use('/api/books', booksRouter)
+
 app.listen(5000, () => console.log('Server Started'))
 
---
+++ server/routes/books.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req,res) => {
+  res.send('Hello get')
+})
+
+router.get('/:id', (req,res) => {
+  res.send(req.params.id)
+})
+
+router.post('/', (req,res) => {
+
+})
+
+router.patch('/', (req,res) => {
+
+})
+router.delete('/:id', (req,res) => {
+
+})
+
+module.exports = router
