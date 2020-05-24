4dfed68a58a099f6d7c5f8aa0ec81aa1104b3c40
Wojciech Knott <wyoyo7@gmail.com>
2020-05-24 20:06:46 +0200

    Add Topbar component

---
+++ client/src/Components/Topbar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import logo from '../book.svg'
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Topbar() {
+  const classes = useStyles()
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <img src={logo} alt='logo' width="50" height="50"/>
+          </IconButton>
+          <Typography variant="h5" className={classes.title}>
+            Lista książek
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
