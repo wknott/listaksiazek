7cc299182866e6a43d52955162d4b109131bf1d1
Wojciech Knott <wyoyo7@gmail.com>
2020-05-24 13:59:57 +0200

    Add mongoDB connection

---
+++ server/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to MongoDB'))
+app.listen(5000, () => console.log('Server Started'))
+
