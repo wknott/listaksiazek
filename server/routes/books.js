f617f9e5ff03c401e3e7960d4243842c5cb72757
Wojciech Knott <wyoyo7@gmail.com>
2020-05-24 16:18:49 +0200

    Add connection to mongodb in books routes

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,23 +1,68 @@
 const express = require('express')
 const router = express.Router()
-
-router.get('/', (req,res) => {
-  res.send('Hello get')
+const Book = require('../models/book')
+router.get('/', async (req,res) => {
+  try {
+    const books = await Book.find()
+    res.json(books)
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
 })
 
-router.get('/:id', (req,res) => {
-  res.send(req.params.id)
+router.get('/:id', getBook, (req,res) => {
+  res.send(res.book.name)
 })
 
-router.post('/', (req,res) => {
-
+router.post('/', async(req,res) => {
+  const book = new Book({
+    name: req.body.name,
+    author: req.body.author,
+  })
+  try{
+    const newBook = await book.save()
+    res.status(201).json(newBook)
+  } catch (err){
+    res.status(400).json({message: err.message})
+  }
 })
 
-router.patch('/', (req,res) => {
-
+router.patch('/:id', getBook, async (req,res) => {
+  if (req.body.name !== null){
+    res.book.name = req.body.name
+  }
+  if (req.body.author !== null){
+    res.book.author = req.body.author
+  }
+  try {
+    const updatedBook = await res.book.save()
+    res.json(updatedBook)
+  } catch (err) {
+    res.status(400).json( {message: err.message})
+  }
 })
-router.delete('/:id', (req,res) => {
 
+router.delete('/:id',getBook, async (req,res) => {
+  try {
+    await res.book.remove()
+    res.json({message: 'Deleted book'})
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
 })
 
+async function getBook(req,res,next) {
+  let book
+  try{
+    book = await Book.findById(req.params.id)
+    if (book === null) {
+      return res.status(404).json({message: 'Nie ma takiej książki'})
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message})
+  }
+
+  res.book = book
+  next()
+}
 module.exports = router
