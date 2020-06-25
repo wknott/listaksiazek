492dee59b58f1f31c34edbaf1bd89552af7f66d3
Wojciech Knott <wojciechknott@gmail.com>
2020-06-25 17:26:40 +0200

    Add sortBooks function

--- client/src/App.js
+++ client/src/App.js
@@ -13,14 +13,34 @@ function App() {
   const [books, setBooks] = useState([]);
   const [selectedYear, setSelectedYear] = useState("");
   const [value, setValue] = useState(0);
+  function compareObjects(key, order = "asc") {
+    return function innerSort(a, b) {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        return 0;
+      }
+      const varA = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
+      const varB = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
+      let comparison = 0;
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
+      return order === "desc" ? comparison * -1 : comparison;
+    };
+  }
   async function loadBooks() {
     try {
       const res = await fetch("/api/books");
       const newBooks = await res.json();
-      setBooks(newBooks);
+      sortBooks(newBooks, "name");
     } catch (err) {
       return err;
     }
+  }
+  function sortBooks(books, key) {
+    const sortedBooks = books.sort(compareObjects(key));
+    setBooks(sortedBooks);
   }
   useEffect(() => {
     loadBooks();
