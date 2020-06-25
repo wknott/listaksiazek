ff69a77f6ae15b7dfc7e1aaaedc1252b718e7fd7
Wojciech Knott <wojciechknott@gmail.com>
2020-06-25 17:31:33 +0200

    Move compareObject function to utilities file

--- client/src/App.js
+++ client/src/App.js
@@ -8,27 +8,12 @@ import YearSelect from "./Components/YearSelect";
 import Container from "@material-ui/core/Container";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-
+import { compareObjects } from "../src/utilities";
 function App() {
   const [books, setBooks] = useState([]);
   const [selectedYear, setSelectedYear] = useState("");
   const [value, setValue] = useState(0);
-  function compareObjects(key, order = "asc") {
-    return function innerSort(a, b) {
-      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
-        return 0;
-      }
-      const varA = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
-      const varB = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
-      let comparison = 0;
-      if (varA > varB) {
-        comparison = 1;
-      } else if (varA < varB) {
-        comparison = -1;
-      }
-      return order === "desc" ? comparison * -1 : comparison;
-    };
-  }
+
   async function loadBooks() {
     try {
       const res = await fetch("/api/books");
