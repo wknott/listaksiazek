748b48924a6f5d9e3fd15b90540578c384285948
Wojciech Knott <wojciechknott@gmail.com>
2020-07-11 11:40:19 +0200

    corrected header and container styles

--- client/src/Header/index.js
+++ client/src/Header/index.js
@@ -4,8 +4,10 @@ import logo from '../logo512.png';
 
 const Header = () => (
   <header className="header">
-    <img className="header__image" src={logo} alt='logo' width="50" height="50" />
-    <h1 className="header__title">Lista książek</h1>
+    <section className="header__container">
+      <img className="header__image" src={logo} alt='logo' width="50" height="50" />
+      <h1 className="header__title">Lista książek</h1>
+    </section>
   </header>
 )
 
