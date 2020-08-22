8f35b153d9eb3c3eef9bca6057d19eba7d3635f9
Wojciech Knott <wojciechknott@gmail.com>
2020-08-22 12:14:03 +0200

    moved header styles to styled components

--- client/src/Header/index.js
+++ client/src/Header/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import "./styles.css";
+import { StyledHeader, HeaderContainer, Image, Title } from "./styled";
 import logo from '../logo512.png';
 
 const Header = () => (
-  <header className="header">
-    <section className="header__container">
-      <img className="header__image" src={logo} alt='logo' width="50" height="50" />
-      <h1 className="header__title">Lista książek</h1>
-    </section>
-  </header>
+  <StyledHeader>
+    <HeaderContainer>
+      <Image src={logo} alt='logo' width="50" height="50" />
+      <Title>Lista książek</Title>
+    </HeaderContainer>
+  </StyledHeader>
 )
 
 export default Header;
--- client/src/Header/styled.js
+++ client/src/Header/styled.js
@@ -1,20 +1,25 @@
-.header {
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
   background-color: #a4508b;
   background-image: linear-gradient(326deg, #a4508b 0%, #5f0a87 74%);
-}
-.header__container {
+`;
+
+export const HeaderContainer = styled.section`
   display: grid;
   grid-template-columns: 70px auto;
   align-items: center;
   grid-gap: 10px;
-  max-width: 1000px;
+  max-width: 800px;
   margin: 0 auto;
-}
-.header__image {
+`;
+
+export const Image = styled.img`
   margin: 10px;
-}
-.header__title {
+`;
+
+export const Title = styled.h1`
   margin: 0;
   padding: 10px;
   color: white;
-}
+`;
